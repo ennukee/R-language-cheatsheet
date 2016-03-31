5164f2c725f52807e45285ef56d0725d23884daa
Dylan Bowers <dylan.volibowers@gmail.com>
2016-03-31 13:17:03 -0400

    Update part-1-example-1.r

--- part-1-example-1.r
+++ part-1-example-1.r
@@ -4,7 +4,7 @@
 
 # You should be capable of writing this code on your own before the competition
 
-import(dplyr)
+library(dplyr)
 
 tbl1 = c(1, 2, 3, 4, 1, 2, 4, 3, 2, 3, 1, 2, 3)
 tbl1.freq = table(tbl1) # NOTE: *.freq is not a syntactic rule, it can be anything. The key is table(TABLE_NAME)
