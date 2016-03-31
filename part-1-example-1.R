a69145704caafbb2526ec502b1a31b0fd1f43d16
Dylan Bowers <dylan.volibowers@gmail.com>
2016-03-31 12:32:18 -0400

    Create part-1-example-1.R

---
+++ part-1-example-1.R
@@ -0,0 +1,23 @@
+# -------------------------------------------- #
+# Concept: Working with basic group operations #
+# -------------------------------------------- #
+
+# You should be capable of writing this code on your own before the competition
+
+import(dplyr)
+
+tbl1 = c(1, 2, 3, 4, 1, 2, 4, 3, 2, 3, 1, 2, 3)
+tbl1.freq = table(tbl1) # NOTE: *.freq is not a syntactic rule, it can be anything. The key is table(TABLE_NAME)
+
+# Observe this result
+print(tbl1.freq) 
+
+# Import data to a data.frame for dplyr operations (note the column names)
+dta1 = data.frame(tbl1.freq) 
+printt(dta1)
+
+# Apply basic summarizaton operations
+dta1 <- dta1 %>% summarize(sum=sum(Freq), standard_dev=sd(Freq), average=mean(Freq))
+
+# Observe the result of the prior operations
+print(dta1)
