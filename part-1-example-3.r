1c9de06a05cc146ded069053d1c06dd6a7c59175
Dylan Bowers <dylan.volibowers@gmail.com>
2016-03-31 13:11:46 -0400

    Rename part-1-example-2.r to part-1-example-3.r

---
+++ part-1-example-3.r
@@ -0,0 +1,20 @@
+# ---------------------------------------------------- #
+# Concept: Advanced multi-level usage of dplyr methods #
+# ---------------------------------------------------- #
+
+import(dplyr)
+
+author = c('John Smith', 'Dans Game', 'Wut Face', 'Kappa Ross', 'Alma Mater', 'Fak Yu')
+book = c('A 1', 'A 2', 'A 3', 'A 4', 'MyMom', 'Kappa', 'Lmaowtf', 'I WAS IN ALPHA', 'Saiko', 'Goddamnitchris', 'B 1', 'B 2')
+year = c(2015, 2015, 2016, 2016, 2017, 2015, 2016, 2014, 2015, 2016, 2015, 2015)
+sold = c(1, 50, 60, 45, 55, 70, 2, 34, 64, 43, 23, 130)
+rating = c(2.3, 5, 5.2, 5, 5.1, 6, 2.1, 4, 6, 7, 4, 9)
+
+data = data.frame(authors, books, year, sold, rating)
+
+# Group by author, then calculate each author's average rating and book sold counts
+data_set_1 = data %>%
+              group_by(author) %>%
+              summarize(average_rating=mean(rating), average_sold=mean(sold))
+
+print(data_set_1)
