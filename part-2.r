08698c3b648ab009bcbe26c7d05cec54dc55ef77
Dylan Bowers <dylan.volibowers@gmail.com>
2016-04-01 01:31:05 -0400

    Create part-2.r

---
+++ part-2.r
@@ -0,0 +1,8 @@
+# R language cheatsheet
+## Part 2'Tidy'ing up your data with Tidyr
+
+This page is heavily based off of the tidyr section of this article (feel free to read it for more info): http://zevross.com/blog/2015/01/13/a-new-data-processing-workflow-for-r-dplyr-magrittr-tidyr-ggplot2/
+
+[Index page](https://github.com/enragednuke/R-language-cheatsheet/blob/master/README.md)
+
+## ...
